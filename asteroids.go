5cf7090d95059e8be969ec5b23a6dd0481b9e48d
Tobshub <tobanigeorge@gmail.com>
2023-07-21 22:46:38 +0100

    move asteroids

--- asteroids.go
+++ asteroids.go
@@ -1,26 +1,62 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	MAX_ASTEROID_VELOCITY int32 = 8
+	MIN_ASTEROID_VELOCITY int32 = 4
+)
 
 type AsteroidSize int32
 
 const (
-	Small AsteroidSize = iota
-	Medium
-	Large
+	Small  AsteroidSize = 15
+	Medium AsteroidSize = 30
+	Large  AsteroidSize = 60
 )
 
 type Asteroid struct {
 	Size AsteroidSize
 
-	Position     rl.Vector2
-	Velocity     int32
-	Rotation     int32
-	Acceleration uint32
+	Position  rl.Vector2
+	Velocity  float32
+	Direction int32
 }
 
 func (a *Asteroid) Update() {
+	displace_x, displace_y := DisplacementComponents(a.Velocity, a.Direction)
+
+	a.Position.X += displace_x
+	a.Position.Y += displace_y
 }
 
 func (a *Asteroid) Draw() {
+	rl.DrawCircleV(a.Position, float32(a.Size), rl.DarkGray)
+}
+
+func RandomAsteroidPosition(_size AsteroidSize, exclusion_area rl.Rectangle) rl.Vector2 {
+	x := exclusion_area.X
+	y := exclusion_area.Y
+
+	var size float32 = float32(_size)
+
+	for (x >= exclusion_area.X-size && x <= exclusion_area.X+exclusion_area.Width+size) ||
+		(y >= exclusion_area.Y-size && y <= exclusion_area.Y+exclusion_area.Height+size) {
+		x = rand.Float32() * float32(SCREEN_WIDTH)
+		y = rand.Float32() * float32(SCREEN_HEIGHT)
+	}
+
+	return rl.Vector2{X: x, Y: y}
+}
+
+func RandomAsteroidVelocity() float32 {
+	return float32(MIN_ASTEROID_VELOCITY) + float32(MAX_ASTEROID_VELOCITY-MIN_ASTEROID_VELOCITY)*rand.Float32()
+}
+
+func RandomAsteroidDirection() int32 {
+	return int32(rand.Intn(360))
 }
--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 const (
-	SCREEN_WIDTH  int32 = 800
-	SCREEN_HEIGHT int32 = 600
+	SCREEN_WIDTH      int32 = 800
+	SCREEN_HEIGHT     int32 = 600
+	INITIAL_ASTERIODS int   = 3
 )
 
-var PLAYER Player
+var (
+	PLAYER    Player
+	ASTEROIDS []Asteroid
+)
 
 func main() {
 	rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Asteroids")
@@ -31,19 +35,45 @@ func main() {
 func InitGame() {
 	// init player
 	PLAYER = Player{
-		Width:    28,
-		Height:   32,
-		Center:   rl.Vector2{X: float32(SCREEN_WIDTH/2 - PLAYER.Width), Y: float32(SCREEN_HEIGHT/2 - PLAYER.Height)},
-		Velocity: 0,
-		Rotation: 270,
+		Width:     28,
+		Height:    32,
+		Center:    rl.Vector2{X: float32(SCREEN_WIDTH/2 - PLAYER.Width), Y: float32(SCREEN_HEIGHT/2 - PLAYER.Height)},
+		Velocity:  0,
+		Direction: 270,
+	}
+
+	ASTEROIDS = []Asteroid{}
+
+	for i := 0; i < INITIAL_ASTERIODS; i++ {
+		asteroid := Asteroid{
+			Size: Large,
+			Position: RandomAsteroidPosition(Large, rl.Rectangle{
+				X:      PLAYER.Center.X - float32(PLAYER.Width/2),
+				Y:      PLAYER.Center.Y - float32(PLAYER.Height/2),
+				Width:  float32(PLAYER.Width),
+				Height: float32(PLAYER.Height),
+			}),
+			Velocity:  RandomAsteroidVelocity(),
+			Direction: RandomAsteroidDirection(),
+		}
+
+		ASTEROIDS = append(ASTEROIDS, asteroid)
 	}
 }
 
 func DrawGame() {
 	rl.ClearBackground(rl.RayWhite)
 	PLAYER.Draw()
+
+	for _, asteroid := range ASTEROIDS {
+		asteroid.Draw()
+	}
 }
 
 func UpdateGame() {
+	for i := 0; i < len(ASTEROIDS); i++ {
+		ASTEROIDS[i].Update()
+	}
+
 	PLAYER.Update()
 }
